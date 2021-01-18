a151aa1e363988179e9e31098def398cbd5fc8bb
Tod Kurt <todbotdotcom@gmail.com>
2021-01-17 21:03:11 -0800

    small updates

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("hiddump0")]
+[assembly: AssemblyTitle("winhiddump")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("hiddump0")]
+[assembly: AssemblyProduct("winhiddump")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
